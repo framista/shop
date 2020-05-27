d76cabbf20192236579c004eccbe16570000edde
framista <adajab1@gmail.com>
2020-05-27 14:07:16 +0200

    Create section for slider, shoes, instagram, benefit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const menuItems = document.querySelectorAll('.menu p');
 const menuLists = [...document.querySelectorAll('.menu ul')];
 const carriers = document.querySelector('#carriers');
 const socialMedia = document.querySelector('.socialMedia');
+const breakPointSm = 576;
 const breakPointMd = 768;
+const breakPointLg = 992;
+// const breakPointXl = 1200;
 
 const elements = [...menuLists, carriers, socialMedia];
 
@@ -27,5 +30,75 @@ menuItems.forEach((item) => {
       visibleElements.forEach((el) => el.classList.add('hidden'));
       e.target.nextElementSibling.classList.toggle('hidden');
     }
+  });
+});
+
+$(document).ready(function () {
+  $('.slider--shoes').slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: breakPointLg,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: breakPointMd,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: breakPointSm,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $('.slider--instagram').slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: breakPointLg,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: breakPointMd,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: breakPointSm,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
   });
 });
