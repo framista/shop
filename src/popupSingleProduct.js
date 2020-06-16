db86e9448d0813d23e13d453d5522f1a5b49f11d
framista <adajab1@gmail.com>
2020-06-16 12:31:49 +0200

    Add loader or message after clicking on add button in popup

--- src/popupSingleProduct.js
+++ src/popupSingleProduct.js
@@ -20,6 +20,14 @@ function closePopup(popup) {
   overlay.classList.remove('overlay--active');
 }
 
+function closePopupWaiting() {
+  const popupWaiting = document.querySelector('#popup-waiting');
+  popupWaiting.innerHTML = '';
+  popupWaiting.setAttribute('style', `width: 0px; height: 0px`);
+  const popup = document.querySelector('.popup-product');
+  popup.style.overflow = 'auto';
+}
+
 function updateProduct(product) {
   const { name, model, oldPrice, price, saving, grade, img1 } = product;
   document.querySelector('#popup-name').innerText = name;
@@ -29,7 +37,6 @@ function updateProduct(product) {
   document.querySelector('#popup-saving').innerText = saving;
   document.querySelector('#popup-grade').innerText = grade;
   document.querySelectorAll('.popup-img-1').forEach((img) => (img.src = img1));
-  console.log(document.querySelector('#popup-stars'));
   const popupStars = document.querySelector('#popup-stars');
   const popupIconsStar = popupStars.querySelectorAll('i');
   popupIconsStar.forEach((icon) => icon.classList.remove('stars--selected'));
@@ -53,10 +60,17 @@ function getProductData(id) {
 
 openProductPopupButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    const popup = document.querySelector('.popup-product');
-    const idProduct = button.dataset.product;
-    getProductData(idProduct);
-    openPopup(popup);
+    const currentWidth = window.innerWidth;
+    if (currentWidth > 1150) {
+      $('.select__normal--notchoosen').removeClass('hidden');
+      $('.select__normal--choosen').addClass('hidden');
+      const popup = document.querySelector('.popup-product');
+      const popupWaiting = document.querySelector('#popup-waiting');
+      popupWaiting.setAttribute('data-popup-waiting', 'message');
+      const idProduct = button.dataset.product;
+      getProductData(idProduct);
+      openPopup(popup);
+    }
   });
 });
 
@@ -66,6 +80,53 @@ closeProductPopup.addEventListener('click', () => {
 });
 
 overlay.addEventListener('click', () => {
-  const popups = document.querySelectorAll('.popup-product--active');
-  popups.forEach((popup) => closePopup(popup));
+  const popupWaiting = document.querySelector('#popup-waiting');
+  const waitingType = popupWaiting.getAttribute('data-popup-waiting');
+  if (waitingType !== 'loader') {
+    closePopupWaiting();
+    const popups = document.querySelectorAll('.popup-product--active');
+    popups.forEach((popup) => closePopup(popup));
+    closePopupWaiting();
+  }
+});
+
+/*
+  add product to chart - show loader and verify if size is selected
+*/
+$('#popup-product-add').click(function (e) {
+  e.preventDefault();
+  const popupWaiting = document.querySelector('#popup-waiting');
+  const popup = document.querySelector('.popup-product');
+  popup.style.overflow = 'hidden';
+  const { width, height } = popup.getBoundingClientRect();
+  popupWaiting.setAttribute(
+    'style',
+    `width: ${Math.floor(width)}px; height: ${Math.floor(height)}px`
+  );
+  if (!$('.select__normal--notchoosen').hasClass('hidden')) {
+    $('.select__normal--error').removeClass('hidden');
+    popupWaiting.innerHTML = `Proszę wybierz rozmiar <br /> jaki chciałbyś kupić`;
+  } else {
+    popupWaiting.setAttribute('data-popup-waiting', 'loader');
+    popupWaiting.innerHTML = `
+      <svg> 
+        <circle cx="70" cy="70" r="70"></circle>
+      </svg> 
+    `;
+    setTimeout(() => {
+      closePopup(popup);
+      closePopupWaiting();
+    }, 3000);
+  }
+});
+
+/*
+  close overlay with info - only choose size 
+*/
+$('#popup-waiting').click(function () {
+  const popupWaiting = document.querySelector('#popup-waiting');
+  const waitingType = popupWaiting.getAttribute('data-popup-waiting');
+  if (waitingType === 'message') {
+    closePopupWaiting();
+  }
 });
